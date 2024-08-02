a3a872bce9f3ea0b7eadc27af15d70d82697e386
thomasw-dev <thomaswang112@gmail.com>
2024-08-02 11:25:54 +0800

    _Dev / TiledSpriteScaler
    Objects can now adjust scale on the root parent, and the sprite will adjust to it automatically.

---
+++ MagnetPlatformer/Assets/Scripts/_Dev/TiledSpriteScaler.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class TiledSpriteScaler : MonoBehaviour
+{
+    [SerializeField] Transform parent;
+
+    void Update()
+    {
+        if (parent == null) { return; }
+
+        transform.localScale = new Vector3(1 / parent.localScale.x, 1 / parent.localScale.y, 1 / parent.localScale.z);
+        GetComponent<SpriteRenderer>().size = new Vector2(parent.localScale.x, parent.localScale.y);
+    }
+}
