d19dea37b134e44435f409557c1a78ea9d58ad79
thomasw-dev <thomaswang112@gmail.com>
2024-08-02 11:47:30 +0800

    Before making Debug Manager use Scriptable Objects
    OnValidate the bools are so inaccurate

--- MagnetPlatformer/Assets/Scripts/_Dev/TiledSpriteScaler.cs
+++ MagnetPlatformer/Assets/Scripts/_Dev/TiledSpriteScaler.cs
@@ -10,6 +10,9 @@ public class TiledSpriteScaler : MonoBehaviour
         if (parent == null) { return; }
 
         transform.localScale = new Vector3(1 / parent.localScale.x, 1 / parent.localScale.y, 1 / parent.localScale.z);
+
+        if (GetComponent<SpriteRenderer>() == null) { return; }
+
         GetComponent<SpriteRenderer>().size = new Vector2(parent.localScale.x, parent.localScale.y);
     }
 }
