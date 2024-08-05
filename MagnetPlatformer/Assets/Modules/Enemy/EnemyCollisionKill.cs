c4c7d4588b22fe9a63594aedf4a721299ee2c1fe
thomasw-dev <thomaswang112@gmail.com>
2024-08-05 12:31:46 +0800

    Reorder tags and update tag users
    Tag 0 - Debug
    Tag 1 - Environment
    Tag 2 - Prop Object
    Tag 3 - Magnetic Object
    Tag 4 - Enemy

--- MagnetPlatformer/Assets/Modules/Enemy/EnemyCollisionKill.cs
+++ MagnetPlatformer/Assets/Modules/Enemy/EnemyCollisionKill.cs
@@ -2,5 +2,9 @@ using UnityEngine;
 
 public class EnemyCollisionKill : MonoBehaviour
 {
+    [Header("Dependencies")] // Required to be assigned in the Inspector
+
+    [SerializeField] CollisionList _collisionList;
+
 
 }
