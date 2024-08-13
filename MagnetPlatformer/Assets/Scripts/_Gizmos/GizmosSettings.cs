c65599ea43765f37358b08d1c1cd7be381698b7b
thomasw-dev <thomaswang112@gmail.com>
2024-08-13 12:45:37 +0800

    GizmosSettingsControl working

---
+++ MagnetPlatformer/Assets/Scripts/_Gizmos/GizmosSettings.cs
@@ -0,0 +1,14 @@
+public static class GizmosSettings
+{
+    public struct MagneticInteraction
+    {
+        public static bool AppliedForces = true;
+        public static bool NetAppliedForce = true;
+        public static bool EmissionRadius = true;
+    }
+
+    public struct Enemy
+    {
+        public static bool TargetPoint = true;
+    }
+}
---
+++ MagnetPlatformer/Assets/Scripts/_Gizmos/GizmosSettingsControl.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Current Settings", order = Constants.GIZMOS_SETTINGS_CONTROL)]
+public class GizmosSettingsControl : ScriptableObject
+{
+    [Header("Magnetic Interaction")]
+    [SerializeField] bool _appliedForces;
+    [SerializeField] bool _netAppliedForce;
+    [SerializeField] bool _emissionRadius;
+
+    [Header("Enemy")]
+    [SerializeField] bool _targetPoint;
+
+    void OnValidate()
+    {
+        GizmosSettings.MagneticInteraction.AppliedForces = _appliedForces;
+        GizmosSettings.MagneticInteraction.NetAppliedForce = _netAppliedForce;
+        GizmosSettings.MagneticInteraction.EmissionRadius = _emissionRadius;
+
+        GizmosSettings.Enemy.TargetPoint = _targetPoint;
+    }
+}
