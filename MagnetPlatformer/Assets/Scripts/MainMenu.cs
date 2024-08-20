393cca91840a5b18dcdb06c5d131383210274d0f
thomasw-dev <thomaswang112@gmail.com>
2024-08-20 15:19:32 +0800

    Main Menu basic (no function)
    Added main menu to build index.

---
+++ MagnetPlatformer/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(0.1f);
+        GameState.ChangeState(GameState.Play);
+    }
+}
