0823ed520e7d020a047f2cc0174c3669ec47de2d
thomasw-dev <thomaswang112@gmail.com>
2024-08-21 17:47:20 +0800

    Scene entry effect

---
+++ MagnetPlatformer/Assets/Scripts/_Specifics/SceneEntryEffect.cs
@@ -0,0 +1,31 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneEntryEffect : MonoBehaviour
+{
+    RawImage _rawImage;
+    float _alpha = 1f;
+    [SerializeField] float _duration = 1f;
+
+    void Awake()
+    {
+        _rawImage = GetComponent<RawImage>();
+    }
+
+    void Start()
+    {
+        if (_rawImage == null) { return; }
+
+        DOTween.To(x => _alpha = x, 1, 0, 1)
+        .SetEase(Ease.OutCirc)
+        .OnUpdate(() =>
+        {
+            _rawImage.color = new Color(_rawImage.color.r, _rawImage.color.g, _rawImage.color.b, _alpha);
+        })
+        .OnComplete(() =>
+        {
+            Destroy(this);
+        });
+    }
+}
