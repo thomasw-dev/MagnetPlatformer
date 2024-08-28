56fcbfc09846ff605aa02b122dfce6406f7d4a6a
thomasw-dev <thomaswang112@gmail.com>
2024-08-28 14:54:37 +0800

    Implement enemy spike death

---
+++ MagnetPlatformer/Assets/Modules/Enemy/EnemySpikeDeath.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemySpikeDeath : MonoBehaviour
+{
+    [Header("Dependencies")] // Required to be assigned in the Inspector
+    [SerializeField] Transform _rootParent;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (!collider.TryGetComponent(out SpikeTiles spikeTiles)) { return; }
+
+        StartCoroutine(WaitSetInactive());
+    }
+
+    IEnumerator WaitSetInactive()
+    {
+        yield return new WaitForSeconds(0.1f);
+        _rootParent.gameObject.SetActive(false);
+    }
+}
---
+++ MagnetPlatformer/Assets/Scripts/_Specifics/SpikeTiles.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+/// <summary>
+/// This class is to define its users that they are spike tiles. (TryGetComponent<> identification)
+/// </summary>
+public class SpikeTiles : MonoBehaviour
+{
+
+}
